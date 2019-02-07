776a1311e6d22ca9f70044d6e86bc2a7c0067d4c
Mohamed Bouhlel <mohamed.bouhleel@gmail.com>
2019-02-07 14:01:21 +0100

    mm

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :sessions
   get '/', to: 'users#index', as: 'index'
   resources :comments
   resources :gossips
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_07_093545) do
+ActiveRecord::Schema.define(version: 2019_02_07_124204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,11 @@ ActiveRecord::Schema.define(version: 2019_02_07_093545) do
     t.datetime "updated_at", null: false
     t.index ["recipient_id"], name: "index_privatemessages_on_recipient_id"
     t.index ["sender_id"], name: "index_privatemessages_on_sender_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "taggossips", force: :cascade do |t|
